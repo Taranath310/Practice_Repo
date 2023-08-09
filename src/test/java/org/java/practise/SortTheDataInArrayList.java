fc58f9332eacc255857bcbfe7029aebc60bbad63
Taranath310 <sachin.taranath1998r@gmail.com>
2023-08-09 18:25:52 +0530

    adding program related to collections

---
+++ src/test/java/org/java/practise/SortTheDataInArrayList.java
@@ -0,0 +1,29 @@
+package org.java.practise;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class SortTheDataInArrayList {
+    public static void main(String[] args) {
+
+
+        ArrayList al=new ArrayList();
+        al.add("X");
+        al.add("Y");
+        al.add("Z");
+        al.add("B");
+        al.add("A");
+        System.out.println("Before sorting : "+al);
+
+        //sort the data
+        Collections.sort(al);
+        System.out.println("After sorting : "+al);
+
+
+        //sort the data in reverseOrder
+        Collections.sort(al,Collections.reverseOrder());
+        System.out.println("After sorting in revers order: "+al);
+
+    }
+}
